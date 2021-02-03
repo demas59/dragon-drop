7695b7b5db41422895388a68951df99883f236fe
unknown <1ttoommyy@gmail.com>
2021-02-03 23:54:07 +0100

    add password encryption, correct some front, add features on login and register

--- front/src/Login.js
+++ front/src/Login.js
@@ -1,31 +1,65 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import {generate as generateHash, verify as verifyHash} from 'password-hash'
 
 export default function Login() {
+    const [isLoading, setIsLoading] = useState(false);
+    const username = useRef();
+    const password = useRef();
+
+    function handleSubmitLogin(event) {
+		event.preventDefault();
+        setIsLoading(true);
+
+        var hashedPassword = generateHash('toto'); //remplacer par un get du password en bdd
+        console.log(verifyHash(password.current.value, hashedPassword));
+        
+		/*const body = JSON.stringify({
+			title: titleInput.current.value,
+			description: descriptionInput.current.value,
+			thumbnail: thumbnailInput.current.value,
+		});*/
+		//fetch(`http://localhost:8080/api/videos`, { method: 'POST', body })
+		//	.then(response => response.json())
+		//	.then(({ id }) => history.push(`/videos/${id}`));
+	}
+
 	return (
         <div className="container mt-5">
-            <div class="row justify-content-md-center">
-                <div class="col-md-5">
-                    <div class="card p-4">
-                        <div className="text-center mb-3">
-                            <h1>Login</h1>
-                        </div>
-                        <form>
-                            <p class="text-muted"> Please enter your login and password!</p>
-                            <div class="form-group">
-                                <label for="emainInputLogin">Username</label>
-                                <input type="email" class="form-control" id="emainInputLogin" aria-describedby="emailHelp" placeholder="Enter username"></input>
-                            </div>
-                            <div class="form-group">
-                                <label for="passwordInputLogin">Password</label>
-                                <input type="password" class="form-control" id="passwordInputLogin" placeholder="Password"></input>
-                            </div>
-                            <div className="d-flex justify-content-between mt-4">
-                                <NavLink class="nav-link pl-0" to="/register">Create account</NavLink>
-                                <button type="submit" class="btn btn-primary">Login</button>
-                            </div>
-                        </form>
+            <div className="col-md-5 mx-auto">
+                <div className="card p-4">
+                    <div className="text-center mb-3">
+                        <h1>Login</h1>
                     </div>
+                    <form onSubmit={event => handleSubmitLogin(event)}>
+                        <p className="text-muted"> Please enter your login and password!</p>
+                        <div className="form-group">
+                            <label htmlFor="usernameInputLogin">Username</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="usernameInputLogin"
+                                placeholder="Enter username"
+                                readOnly = {isLoading}
+                                ref={username}
+                            ></input>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="passwordInputLogin">Password</label>
+                            <input
+                                type="password"
+                                className="form-control"
+                                id="passwordInputLogin"
+                                placeholder="Password"
+                                readOnly = {isLoading}
+                                ref={password}
+                            ></input>
+                        </div>
+                        <div className="d-flex justify-content-between mt-4">
+                            <NavLink className="nav-link pl-0" to="/register">Create account</NavLink>
+                            <button type="submit" className="btn btn-primary" disabled={isLoading}>{!isLoading ? 'Login' : 'Loading...'}</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
--- front/src/Register.js
+++ front/src/Register.js
@@ -1,67 +1,97 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import {generate as generateHash, verify as verifyHash} from 'password-hash'
 
 export default function Register() {
-    const [password, setPassword] = useState(``);
-    const [passwordConfirm, setPasswordConfirm] = useState(``);
+    const username = useRef();
+    const [validUsername, setValidUsername] = useState(true);
+    const password = useRef();
+    const passwordConfirm = useRef();
+    const [passwordsMatch, setPasswordMatch] = useState(true);
+    const [isLoading, setIsLoading] = useState(false);
 
-    const [passwordsMatch, setPasswordMatch] = useState(``);
-
-    function handleInputPasswordChange(event) {
+    function handleInputUsername(event) {
         event.preventDefault();
-        setPassword(event.target.value);
-        event.target.value === passwordConfirm ? setPasswordMatch("") : setPasswordMatch("is-invalid");
+        if(false) { //si le username est déjà pris
+            setValidUsername(false);
+        }
 	}
 
-    function handleInputPasswordConfirmChange(event) {
+    function checkMatchingPasswords(event) {
         event.preventDefault();
-		setPasswordConfirm(event.target.value);
-        password === event.target.value ? setPasswordMatch("") : setPasswordMatch("is-invalid");
+        setPasswordMatch(password.current.value === passwordConfirm.current.value ? true : false);
+	}
+    
+    function handleSubmitRegister(event) {
+		event.preventDefault();
+        //setIsLoading(true);
+
+        if(!passwordsMatch) {return;}
+
+        var hashedPassword = generateHash(password);
+
+        /*const body = JSON.stringify({
+			title: titleInput.current.value,
+			description: descriptionInput.current.value,
+			thumbnail: thumbnailInput.current.value,
+		});*/
+		//fetch(`http://localhost:8080/api/videos`, { method: 'POST', body })
+		//	.then(response => response.json())
+		//	.then(({ id }) => history.push(`/videos/${id}`));
 	}
 
 	return (
         <div className="container mt-5">
-            <div className="row justify-content-md-center">
-                <div className="col-md-5">
-                    <div className="card p-4">
-                        <div className="text-center mb-3">
-                            <h1>Register</h1>
-                        </div>
-                        <form>
-                            <p className="text-muted"> Please enter your login and password!</p>
-                            <div className="form-group">
-                                <label for="emainInputLogin">Username</label>
-                                <input type="email" className="form-control" id="emainInputLogin" aria-describedby="emailHelp" placeholder="Enter username"></input>
-                            </div>
-                            <div className="form-group">
-                                <label for="passwordInputLogin">Password</label>
-                                <input
-                                    type="password"
-                                    value={password}
-                                    onChange={handleInputPasswordChange}
-                                    className="form-control"
-                                    id="passwordInputLogin"
-                                    placeholder="Password"
-                                ></input>
-                            </div>
-                            <div className="form-group">
-                                <label for="confirmPasswordInputLogin">Confirm password</label>
-                                <input
-                                    type="password"
-                                    value={passwordConfirm}
-                                    onChange={handleInputPasswordConfirmChange}
-                                    className={"form-control "+passwordsMatch}
-                                    id="confirmPasswordInputLogin"
-                                    placeholder="Confirm password"
-                                ></input>
-                                <div className="invalid-feedback">Passwords do not match.</div>
-                            </div>
-                            <div className="d-flex justify-content-between mt-4">
-                                <NavLink className="nav-link pl-0" to="/login">I have an account</NavLink>
-                                <button type="submit" className="btn btn-primary">Register</button>
-                            </div>
-                        </form>
+            <div className="col-md-5 mx-auto">
+                <div className="card p-4">
+                    <div className="text-center mb-3">
+                        <h1>Register</h1>
                     </div>
+                    <form onSubmit={event => handleSubmitRegister(event)}>
+                        <p className="text-muted"> Please enter your login and password!</p>
+                        <div className="form-group">
+                            <label htmlFor="usernameInput">Username</label>
+                            <input
+                                type="text"
+                                ref={username}
+                                onChange={handleInputUsername}
+                                className={"form-control "+ (validUsername ? "" : "is-invalid")}
+                                id="usernameInput"
+                                placeholder="Enter username"
+                                readOnly = {isLoading}
+                            ></input>
+                            <div className="invalid-feedback">Username already used.</div>
+                        </div>
+                        <div className="form-group mt-2">
+                            <label htmlFor="passwordInputLogin">Password</label>
+                            <input
+                                type="password"
+                                ref={password}
+                                onChange={checkMatchingPasswords}
+                                className="form-control"
+                                id="passwordInputLogin"
+                                placeholder="Password"
+                                readOnly = {isLoading}
+                            ></input>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="confirmPasswordInputLogin">Confirm password</label>
+                            <input
+                                type="password"
+                                ref={passwordConfirm}
+                                onChange={checkMatchingPasswords}
+                                className={"form-control "+ (passwordsMatch ? "" : "is-invalid")}
+                                id="confirmPasswordInputLogin"
+                                placeholder="Confirm password"
+                                readOnly = {isLoading}
+                            ></input>
+                            <div className="invalid-feedback">Passwords do not match.</div>
+                        </div>
+                        <div className="d-flex justify-content-between mt-4">
+                            <NavLink className="nav-link pl-0" to="/login">I have an account</NavLink>
+                            <button type="submit" className="btn btn-primary" disabled={isLoading}>{!isLoading ? 'Register' : 'Loading...'}</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
