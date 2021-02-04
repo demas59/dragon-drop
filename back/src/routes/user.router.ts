ac95901da6f5df4372e8a34de97686ac66d32721
demas59 <demasay@gmail.com>
2021-02-04 21:50:21 +0100

    back fonctionnel + fonctionnement mongo locale

---
+++ back/src/routes/user.router.ts
@@ -0,0 +1,24 @@
+import express, { Request, Response } from "express";
+import { User } from "../models/user.model";
+import HttpStatusCodes from "http-status-codes";
+
+const router = express.Router();
+
+router.get("/user", async (req: Request, res: Response) => {
+  const users = await User.find({});
+  return res.send(users);
+});
+
+router.post("/user", async (req: Request, res: Response) => {
+  const newUser = new User(req.body);
+
+  try {
+    const createdUser = await newUser.save();
+    res.status(HttpStatusCodes.ACCEPTED).send(createdUser);
+  } catch (err) {
+    console.error(err.message);
+    res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).send(err.message);
+  }
+});
+
+export { router as userRouter };
