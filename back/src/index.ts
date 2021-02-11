b8ded1c5ebe46b7757c302dbea397584ffc60de0
demas59 <demasay@gmail.com>
2021-02-11 14:15:13 +0100

    add(back): ajout cors origin

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import { json } from "body-parser";
 import { userRouter } from "./routes/user.router";
 import { postRouter } from "./routes/post.router";
 import mongoose from "mongoose";
 
 const app = express();
+app.use(cors());
 app.use(json());
 
 //Implementation des routes
@@ -12,17 +14,18 @@ app.use(userRouter);
 app.use(postRouter);
 
 //Configuration connexion mongoDb
-mongoose.connect(
-  // "mongodb://localhost:27017/dragondrop",
-  "mongodb+srv://ademas:ademas@cluster0.pr6cz.mongodb.net/dragondrop?retryWrites=true&w=majority",
-  {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-)     .then(() => console.log( 'Database Connected' ))
-.catch(err => console.log( err ));
-
+mongoose
+  .connect(
+    // "mongodb://localhost:27017/dragondrop",
+    "mongodb+srv://ademas:ademas@cluster0.pr6cz.mongodb.net/dragondrop?retryWrites=true&w=majority",
+    {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("Database Connected"))
+  .catch((err) => console.log(err));
 
 app.listen(3000, () => {
   console.log("server is listening on port 3000");
